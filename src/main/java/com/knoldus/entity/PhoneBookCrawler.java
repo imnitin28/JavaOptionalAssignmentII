a117e742ddc5e297dc79184e79af23038245ddb2
Nitin Mishra <nitin.mishra@knoldus.com>
2021-03-22 17:13:12 +0530

    Adding more test cases

--- src/main/java/com/knoldus/entity/PhoneBookCrawler.java
+++ src/main/java/com/knoldus/entity/PhoneBookCrawler.java
@@ -3,88 +3,96 @@ package com.knoldus.entity;
 import java.util.Map;
 import java.util.stream.Stream;
 /***
- * This class uses phonebook and performs various functions.
+ * <p>This class uses phonebook and performs various functions.</p>
  */
+
 public class PhoneBookCrawler {
-    /***
-     *
-     */
-    private PhoneBook phoneBook;
-    /***
-     * constructor that takes PhoneBook object as an argument.
-     * @param phoneBook1
-     */
-    public PhoneBookCrawler(final PhoneBook phoneBook1) {
-        this.phoneBook = phoneBook1;
-    }
+  /***
+  * <p>private instance of PhoneBook class.</p>
+  */
 
-    /***
-     *
-     * @param name
-     * @return stringMessage
-     */
-    public String findPhoneByNameAndPunishIfNothingFound(final String name) {
-        try {
-            return phoneBook.findPhoneByName(name)
-                    .orElseThrow(() -> new IllegalArgumentException("Punish"));
-        } catch (IllegalArgumentException illegalArgumentException) {
-            return "Punish";
-        }
-    }
-    /***
-     *
-     * @param name
-     * @return phone or phone book
-     */
-    public String findPhoneByNameAndPrintPhoneBookIfNothingFound(
-            final String name) {
-        return phoneBook.findPhoneByName(name)
-                .orElseGet(() -> String.valueOf(phoneBook.getPhoneDirectory()));
-    }
-    /***
-     *
-     * @param name
-     * @return phone or punish message
-     */
-    public String findPhoneByNameAndPunishIfNothingFoundUsingStreams(
-            final String name) {
-        Map<String, String> phoneBookList = getPhoneBook().getPhoneDirectory();
-        return Stream.of(phoneBookList)
-                .anyMatch(phoneBookList1 -> phoneBookList
-                .containsKey(name)) ? phoneBookList.get(name) : "Punish";
-    }
+  private PhoneBook phoneBook;
+  /***
+   * <p>constructor that takes PhoneBook object as an argument.</p>
+   * @param phoneBook1 PhoneBook instance.
+   */
 
-    /***
-     *
-     * @param name
-     * @return phone or phone book
-     */
-    public String findPhoneByNameAndPrintPhoneBookIfNothingFoundStream(
-            final String name) {
-        Map<String, String> phoneBookList = getPhoneBook().getPhoneDirectory();
-        return Stream.of(phoneBookList)
-                .anyMatch(phoneBookList1 -> phoneBookList.containsKey(name))
-                ? phoneBookList.get(name) : phoneBookList.toString();
-    }
+  public PhoneBookCrawler(final PhoneBook phoneBook1) {
+    this.phoneBook = phoneBook1;
+  }
 
-    /***
-     *
-     * @param name
-     * @param phoneNumber
-     * @return phone or name
-     */
-    public String findPhoneNumberByNameOrNameByPhoneNumber(
-            final String name, final String phoneNumber) {
-        return name.length() == 0 ? phoneBook
+  /***
+   * <p>attempts to find phone number using name and punish if not found.</p>
+   * @param name name of the user.
+   * @return stringMessage
+   */
+
+  public String findPhoneByNameAndPunishIfNothingFound(final String name) {
+    try {
+      return phoneBook.findPhoneByName(name)
+              .orElseThrow(() -> new IllegalArgumentException("Punish"));
+    } catch (IllegalArgumentException illegalArgumentException) {
+      return "Punish";
+    }
+  }
+  /***
+   * <p>find phone number using name and if not found prints phoneBook.</p>
+   * @param name name of the user.
+   * @return phone or phone book
+   */
+
+  public String findPhoneByNameAndPrintPhoneBookIfNothingFound(
+          final String name) {
+    return phoneBook.findPhoneByName(name)
+          .orElseGet(() -> String.valueOf(phoneBook.getPhoneDirectory()));
+  }
+  /***
+   * <p>attempts to find phone number using name and punish if not found.</p>
+   * @param name name of the user
+   * @return phone or punish message
+   */
+
+  public String findPhoneByNameAndPunishIfNothingFoundUsingStreams(
+          final String name) {
+    Map<String, String> phoneBookList = getPhoneBook().getPhoneDirectory();
+    return Stream.of(phoneBookList)
+            .anyMatch(phoneBookList1 -> phoneBookList
+            .containsKey(name)) ? phoneBookList.get(name) : "Punish";
+  }
+
+  /***
+   * <p>find phone number using name and if not found prints phoneBook.</p>
+   * @param name name of the user.
+   * @return phone or phone book
+   */
+
+  public String findPhoneByNameAndPrintPhoneBookIfNothingFoundStream(
+          final String name) {
+    Map<String, String> phoneBookList = getPhoneBook().getPhoneDirectory();
+    return Stream.of(phoneBookList)
+            .anyMatch(phoneBookList1 -> phoneBookList.containsKey(name))
+            ? phoneBookList.get(name) : phoneBookList.toString();
+  }
+  /***
+   * <p>attempts to find phone number using name or vice versa..</p>
+   * @param name name of the user.
+   * @param phoneNumber phone number of the user.
+   * @return phone or name of the user as per input.
+   */
+
+  public String findPhoneNumberByNameOrNameByPhoneNumber(
+          final String name, final String phoneNumber) {
+    return name.length() == 0 ? phoneBook
                 .findNameByPhoneNumber(phoneNumber)
                 .get() : phoneBook.findPhoneByName(name).get();
-    }
+  }
 
-    /***
-     *
-     * @return phonebook
-     */
-    public PhoneBook getPhoneBook() {
-        return phoneBook;
-    }
+  /***
+   * <p>method to get phone book.</p>
+   * @return phonebook all phone book records.
+   */
+
+  public PhoneBook getPhoneBook() {
+    return phoneBook;
+  }
 }
---
+++ src/test/java/com/knoldus/test/PhoneBookCrawlerTest.java
@@ -0,0 +1,71 @@
+package com.knoldus.test;
+
+import com.knoldus.entity.PhoneBook;
+import com.knoldus.entity.PhoneBookCrawler;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PhoneBookCrawlerTest {
+    PhoneBook phoneBook = new PhoneBook();
+    PhoneBookCrawler phoneBookCrawler = new PhoneBookCrawler(phoneBook);
+    @Test
+    public void findPhoneByNameAndPunishIfNothingFoundTest()
+    {
+        String phoneNumberReceived = phoneBookCrawler.findPhoneByNameAndPunishIfNothingFound("Nitin");
+        Assert.assertEquals(phoneNumberReceived,"01111111111");
+    }
+    @Test
+    public void findPhoneByNameAndPunishIfNothingFoundTest1()
+    {
+        String phoneNumberReceived = phoneBookCrawler.findPhoneByNameAndPunishIfNothingFound("Johnny");
+        Assert.assertEquals(phoneNumberReceived,"Punish");
+    }
+    @Test
+    public void findPhoneByNameAndPrintPhoneBookIfNothingFoundTest()
+    {
+        String phoneNumberReceived = phoneBookCrawler.findPhoneByNameAndPrintPhoneBookIfNothingFound("Sonam");
+        Assert.assertEquals(phoneNumberReceived,"02222222222");
+    }
+    @Test
+    public void findPhoneByNameAndPrintPhoneBookIfNothingFoundTest1()
+    {
+        String phoneNumberReceived = phoneBookCrawler.findPhoneByNameAndPrintPhoneBookIfNothingFound("Amish");
+        Assert.assertEquals(phoneNumberReceived,"{Nitin=01111111111, Ankush=05555555555, Sonam=02222222222, Ayush=03333333333, Rajdeep=04444444444}");
+    }
+    @Test
+    public void findPhoneByNameAndPunishIfNothingFoundUsingStreamsTest()
+    {
+        String phoneNumberReceived = phoneBookCrawler.findPhoneByNameAndPunishIfNothingFoundUsingStreams("Mike");
+        Assert.assertEquals(phoneNumberReceived,"Punish");
+    }
+    @Test
+    public void findPhoneByNameAndPunishIfNothingFoundUsingStreamsTest1()
+    {
+        String phoneNumberReceived = phoneBookCrawler.findPhoneByNameAndPunishIfNothingFoundUsingStreams("Johnny");
+        Assert.assertEquals(phoneNumberReceived,"Punish");
+    }
+    @Test
+    public void findPhoneByNameAndPrintPhoneBookIfNothingFoundUsingStreamsTest()
+    {
+        String phoneNumberReceived = phoneBookCrawler.findPhoneByNameAndPrintPhoneBookIfNothingFoundStream("Rajdeep");
+        Assert.assertEquals(phoneNumberReceived,"04444444444");
+    }
+    @Test
+    public void findPhoneByNameAndPrintPhoneBookIfNothingFoundUsingStreamsTest1()
+    {
+        String phoneNumberReceived = phoneBookCrawler.findPhoneByNameAndPrintPhoneBookIfNothingFoundStream("Mike");
+        Assert.assertEquals(phoneNumberReceived,"{Nitin=01111111111, Ankush=05555555555, Sonam=02222222222, Ayush=03333333333, Rajdeep=04444444444}");
+    }
+    @Test
+    public void findPhoneNumberByNameOrNameByPhoneNumberTest()
+    {
+        String phoneNumberByName = phoneBookCrawler.findPhoneNumberByNameOrNameByPhoneNumber("Nitin","");
+        Assert.assertEquals(phoneNumberByName,"01111111111");
+    }
+    @Test
+    public void findPhoneNumberByNameOrNameByPhoneNumberTest1()
+    {
+        String nameByPhoneNumber = phoneBookCrawler.findPhoneNumberByNameOrNameByPhoneNumber("","01111111111");
+        Assert.assertEquals(nameByPhoneNumber,"Nitin");
+    }
+}
